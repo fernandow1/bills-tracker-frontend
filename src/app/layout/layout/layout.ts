e01edf21d1ee70d6a0e233b50b5f0ba06669c92a
Fernando Wabab <wababfernando@gmail.com>
2025-12-04 22:06:52 -0300

    feat: remove temporary filler content from layout component

--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -28,16 +28,6 @@ export class Layout implements OnDestroy {
 
   protected readonly navOptions = Array.from({ length: 5 }, (_, i) => `Nav Option ${i + 1}`);
 
-  protected readonly fillerContent = Array.from(
-    { length: 50 },
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-  );
-
   private readonly _mobileQuery: MediaQueryList;
   private readonly _mobileQueryListener: () => void;
   private readonly authService = inject(AuthService);
