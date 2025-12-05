bdc9e1a0814c9f63c9f1fb1792a999fd1faa6384
Fernando Wabab <wababfernando@gmail.com>
2025-12-04 22:42:08 -0300

    feat: add Bill component with HTML, SCSS, and unit tests

---
+++ src/app/features/bill/bill.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bill',
+  imports: [],
+  templateUrl: './bill.html',
+  styleUrl: './bill.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Bill {
+
+}
