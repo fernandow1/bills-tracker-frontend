5b0ec7df3e6e37ce0e43845cb52c54ac3c2640c5
Fernando Wabab <wababfernando@gmail.com>
2026-01-21 16:13:45 -0300

    Paginacion para la lista de cateogrias

--- src/app/core/interfaces/pagination.interface.ts
+++ src/app/core/interfaces/pagination.interface.ts
@@ -1,4 +1,4 @@
 export interface Pagination<T> {
-  count: number;
-  data: T[];
+  count: number; // Total de registros que coinciden con los filtros (no solo los de la página actual)
+  data: T[]; // Registros de la página actual
 }
