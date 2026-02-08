7a21fde98be0bedada872ddea6a737d973a53522
Fernando Wabab <wababfernando@gmail.com>
2026-02-08 15:54:35 -0300

    feat: implement user creation and editing form with validation and reactive signals.

--- src/app/features/user/interfaces/user-data.interface.ts
+++ src/app/features/user/interfaces/user-data.interface.ts
@@ -1,6 +1,9 @@
+import { Role } from '@features/auth/enums/role.enum';
+
 export interface IUserData {
   name: string;
   email: string;
   username: string;
   password?: string; // Opcional para edición
+  role: Role;
 }
